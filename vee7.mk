4f6bd6ce51411eeceb237325d08fa09036776f4c
HostZero <haneesh.da.rocker@gmail.com>
2016-07-02 18:33:24 +0530

    Removed Unwanted Calls

--- vee7.mk
+++ vee7.mk
@@ -1,8 +1,3 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product, vendor/lge/vee7/vee7-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/vee7/overlay
 

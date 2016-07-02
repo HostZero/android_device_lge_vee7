d86803b7db977f2465f5647fb277dc5da23d7d26
HostZero <haneesh.da.rocker@gmail.com>
2016-07-02 18:31:20 +0530

    Removed Unwanted Calls

--- cm.mk
+++ cm.mk
@@ -1,9 +1,6 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/lge/vee7/vee7.mk)
 
